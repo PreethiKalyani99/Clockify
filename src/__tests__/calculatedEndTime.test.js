0c9f364e4032a6bee9a6969f2b996e3413c5ddde
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-05-07 07:08:35 +0530

    adds test for calculatedEndTime

--- src/__tests__/calculatedEndTime.test.js
+++ src/__tests__/calculatedEndTime.test.js
@@ -1,62 +1,89 @@
 import { calculateEndTime } from "../utils/calculateEndTime";
 
 test('calculate end time when total time input change', () => {
-    expect(calculateEndTime('12:00', '02:00', '135:34:77')).toStrictEqual({startTime: '12:00', endTime: "03:35", totalTime: "135:35:17", isValid: true})
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(135 + startDate.getHours(), 35 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '135:34:77')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '135:35:17'})
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00','02:00',  '12')).toStrictEqual({startTime: '12:00', endTime: "12:12", totalTime: "00:12:00", isValid: true}) 
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(0 + startDate.getHours(), 12 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'),  '12')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '00:12:00'}) 
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '1234')).toStrictEqual({startTime: '12:00', endTime: "00:34", totalTime: "12:34:00", isValid: true})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(12 + startDate.getHours(), 34 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '1234')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '12:34:00'})  
+})
+ 
+test('', () => {
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(123 + startDate.getHours(), 45 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'),  '12345')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '123:45:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00',  '12345')).toStrictEqual({startTime: '12:00', endTime: "15:45", totalTime: "123:45:00", isValid: true})  
-})
-
-
-test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '123456')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(999 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '123456')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '999:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '123:99999:00')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(999 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '123:99999:00')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '999:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '12334:12:00')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(999 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '12334:12:00')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '999:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '123:99:0045')).toStrictEqual({startTime: '12:00', endTime: "16:39", totalTime: "124:39:45", isValid: true})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(124 + startDate.getHours(), 39 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '123:99:0045')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '124:39:45'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '45:12:0087656')).toStrictEqual({startTime: '12:00', endTime: "09:32", totalTime: "69:32:56", isValid: true})   
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(69 + startDate.getHours(), 32 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '45:12:0087656')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '69:32:56'})   
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', 'dsajfhkjdf')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), 'dsajfhkjdf')).toStrictEqual({isValid: false})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00','02:00', '12:-4:6')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(999 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '12:-4:6')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '999:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00','02:00',  '12:45:0')).toStrictEqual({startTime: '12:00', endTime: "00:45", totalTime: "12:45:00", isValid: true})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(12 + startDate.getHours(), 45 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'),  '12:45:0')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '12:45:00'})  
 })
 
 test('', () => { 
-    expect(calculateEndTime('12:00','02:00',  '12:')).toStrictEqual({startTime: '12:00', endTime: "00:00", totalTime: "12:00:00", isValid: true})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(12 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'),  '12:')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '12:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00', '02:00', '1234:567:789')).toStrictEqual({startTime: '12:00', endTime: "02:00", totalTime: "999:00:00", isValid: false})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(999 + startDate.getHours(), 0 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'), '1234:567:789')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '999:00:00'})  
 })
 
 test('', () => {
-    expect(calculateEndTime('12:00','02:00',  ':12:')).toStrictEqual({startTime: '12:00', endTime: "12:12", totalTime: "00:12:00", isValid: true})  
+    const startDate = new Date('2024-05-01 12:00')
+    startDate.setHours(0 + startDate.getHours(), 12 + startDate.getMinutes())
+    expect(calculateEndTime(new Date('2024-05-01 12:00'),  ':12:')).toStrictEqual({isValid: true, newEndTime: startDate, timeDuration: '00:12:00'})  
 }) 
