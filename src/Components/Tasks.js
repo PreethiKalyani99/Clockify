362a20b7dc3e39822653845dae38174ecf7a9337
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-05-15 22:54:11 +0530

    Groups tasks by week

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -7,9 +7,12 @@ import { convertToHoursAndMinutes } from "../utils/convertToHoursAndMinutes";
 import { isDurationLimitExceeded } from "../utils/isDurationLimitExceeded";
 import { calculateEndDate } from "../utils/calculateEndDate";
 import { calculateEndTime } from "../utils/calculateEndTime";
+import { groupTasksByWeek } from "../utils/groupTasksByWeek";
 
 export function Tasks({isSidebarShrunk, tasks, addTodayTask, projectClient, timeStart, timeEnd, uniqueId}){
     const dispatch = useDispatch()
+
+    const tasksByWeek = groupTasksByWeek({ tasks })
 
     function formatDate(dateString) {
         const date = new Date(dateString)
@@ -74,28 +77,39 @@ export function Tasks({isSidebarShrunk, tasks, addTodayTask, projectClient, time
         dispatch(deleteTask({id}))
     }
     return(
-        <div className="parent-container mt-5" >
-            {tasks.length > 0 && <div className="display-container">
-                <div> {tasks.map((task, index) => (
-                    <div className={(tasks.length > 0 && tasks.length-1 !== index) ? isSidebarShrunk ? "sub-container border-style expand-width" : "sub-container border-style shrink-width" : "sub-container"} key={index}>
-                        <Task
-                            key={index}
-                            task={task}
-                            timeStart={timeStart}
-                            timeEnd={timeEnd}
-                            projectClient={projectClient}
-                            onTaskBlur={handleTaskNameBlur}
-                            onStartBlur={handleStartTimeBlur}
-                            onEndBlur={handleEndTimeBlur}
-                            onDurationBlur={handleDurationBlur}
-                            onDateChange={handleDateChange}
-                            onDuplicate={handleDuplicateTask}
-                            onDelete={handleDeleteTask}
-                        />
+        <div className="parent-container" >
+           {Object.entries(tasksByWeek).map(([range, total_tasks]) => {
+             const result = range.split(' to ').map(date => formatDate(date))
+             return (<div key={range} className="mt-3">
+                <p><b>{result.join(' - ')}</b></p>
+                {Object.entries(total_tasks).map(([key, tasks]) => (
+                    <div className="week-container mb-3" key={key}>
+                        <p>{new Date(key).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}</p>
+                        {tasks.length > 0 && <div className="display-container" key={key}>
+                            <div> {tasks.map((task, index) => (
+                                <div className={(tasks.length > 0 && tasks.length-1 !== index) ? isSidebarShrunk ? "sub-container border-style expand-width" : "sub-container border-style shrink-width" : "sub-container"} key={index}>
+                                    <Task
+                                        key={index}
+                                        task={task}
+                                        timeStart={timeStart}
+                                        timeEnd={timeEnd}
+                                        projectClient={projectClient}
+                                        onTaskBlur={handleTaskNameBlur}
+                                        onStartBlur={handleStartTimeBlur}
+                                        onEndBlur={handleEndTimeBlur}
+                                        onDurationBlur={handleDurationBlur}
+                                        onDateChange={handleDateChange}
+                                        onDuplicate={handleDuplicateTask}
+                                        onDelete={handleDeleteTask}
+                                    />
+                                </div>
+                            ))}
+                            </div>
+                        </div>}
                     </div>
                 ))}
-                </div>
-            </div>}
+            </div>)
+        })}
         </div>
     )
 }
---
+++ src/utils/groupTasksByWeek.js
@@ -0,0 +1,66 @@
+import { getFormattedDate } from "./getFormattedDate"
+
+export function groupTasksByWeek({ tasks }){
+    let tasksByWeek = {}
+    tasks && tasks.forEach(task => {
+        const date = getFormattedDate(new Date(task.startTime))
+        let taskDate = new Date(task.startTime)
+
+        let taskDay = taskDate.getDate()
+        let taskYear = taskDate.getFullYear()
+        let taskMonth = taskDate.getMonth() + 1
+
+        let first = taskDate.getDate() - taskDate.getDay()
+        let last = first + 6
+
+        let weekStartDate = new Date(taskDate.setDate(first))
+        let weekEndDate = new Date(taskDate.setDate(last))
+
+        let startDate = weekStartDate.getDate() 
+        let startMonth = weekStartDate.getMonth() + 1
+        let startYear = weekStartDate.getFullYear()
+
+        let endDate = weekEndDate.getDate()
+        let endMonth = weekEndDate.getMonth() + 1 
+        let endYear = weekEndDate.getFullYear() 
+
+        if((endMonth === 12 && taskMonth < endMonth) || (taskDay < startDate && taskMonth > endMonth)){
+            endMonth = taskMonth
+            if(taskYear > endYear){
+                endYear += 1
+            }
+        }
+        else{
+            endYear = weekEndDate.getFullYear()
+            endMonth = weekEndDate.getMonth() + 1
+        }
+
+        let weekRange = `${startYear}-${startMonth}-${startDate} to ${endYear}-${endMonth}-${endDate}`
+
+        let found = false
+        for (const range in tasksByWeek) {
+            let [start, end] = range.split(' to ').map(dateStr => new Date(dateStr))
+            if (taskDate >= start && taskDate <= end) {
+                if (!tasksByWeek[range][date]) {
+                    tasksByWeek[range][date] = []
+                }
+                tasksByWeek[range][date].push(task)
+                found = true
+                break
+            }
+        }
+
+        if (!found) {
+            if (!tasksByWeek[weekRange]) {
+                tasksByWeek[weekRange] = {}
+            }
+
+            if (!tasksByWeek[weekRange][date]) {
+                tasksByWeek[weekRange][date] = []
+            }
+            tasksByWeek[weekRange][date].push(task)
+        }
+    })
+
+    return tasksByWeek
+}
